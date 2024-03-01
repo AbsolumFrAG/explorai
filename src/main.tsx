4075bf32b4dd14c9a8d0c4343b6df47aebf38abc
Lou Tigroudja <lou.tigroudja07@gmail.com>
2024-03-01 12:38:37 +0800

    Added Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./styles/index.css";
+import { inject } from "@vercel/analytics";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = ReactDOM.createRoot(rootElement);
+
+inject();
 
 root.render(
   <Router>
